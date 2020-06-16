607a0b0007f02f87fe06a6b9de00274a2407482a
Paul Joshi <49583322+PaulJoshi@users.noreply.github.com>
2020-06-16 13:29:26 +0530

    First function added - the 'print_board' function which prints the current status of the game board

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,1 +1,12 @@
-
+def print_board() { #Function to print the board
+    board = {
+        '7' : ' ', '8' : ' ', '9' : ' ',
+        '4' : ' ', '5' : ' ', '6' : ' ',
+        '1' : ' ', '2' : ' ', '3' : ' '}
+    print(
+        board['7']+'|'+board['8']+'|'+board['9']+'\n'
+        +'-+-+-'+'\n'+
+        board['4']+'|'+board['5']+'|'+board['6']+'\n'
+        +'-+-+-'+'\n'+
+        board['1']+'|'+board['2']+'|'+board['3'])
+}
