e044957505bd153fa40fc9466b07cf4e1b54a5c6
Paul Joshi <49583322+PaulJoshi@users.noreply.github.com>
2020-06-16 14:28:23 +0530

    Added functions : #1 clrscr - clears the terminal screen, #2 ai_play - returns a random number(1-9) whose position in the board is empty, #3 user_play - Allows user to choose move

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,12 +1,40 @@
-def print_board() { #Function to print the board
-    board = {
-        '7' : ' ', '8' : ' ', '9' : ' ',
-        '4' : ' ', '5' : ' ', '6' : ' ',
-        '1' : ' ', '2' : ' ', '3' : ' '}
+import os
+import random
+
+board = {
+    '7' : ' ', '8' : ' ', '9' : ' ',
+    '4' : ' ', '5' : ' ', '6' : ' ',
+    '1' : ' ', '2' : ' ', '3' : ' '}
+
+def print_board():              #Function to print the board
     print(
-        board['7']+'|'+board['8']+'|'+board['9']+'\n'
-        +'-+-+-'+'\n'+
-        board['4']+'|'+board['5']+'|'+board['6']+'\n'
-        +'-+-+-'+'\n'+
-        board['1']+'|'+board['2']+'|'+board['3'])
-}
+         '\t\t' + board['7'] + '|' + board['8'] + '|' + board['9'] + '\n'
+        +'\t\t' + '-+-+-' + '\n'
+        +'\t\t' + board['4'] + '|' + board['5'] + '|' + board['6'] + '\n'
+        +'\t\t' + '-+-+-' + '\n'
+        +'\t\t' + board['1'] + '|' + board['2'] + '|' + board['3'])
+
+def clrscr():                   #Function to clear terminal viewport
+    os.system('cls||clear')
+
+def ai_play():                  #Function to allow user to choose move
+    while(True):
+        move = random.randint(1,9)
+        if board[str(move)] == ' ':
+            return move
+        else: continue
+
+def user_play():                #Function to allow user to choose move
+    while(True):
+        move = input("Make a move (1-9)")
+        if board[str(move)] == ' ':
+            return move
+        else: 
+            print('Move already made')
+            continue
+
+
+clrscr()
+print_board()
+x = ai_play()
+print(x)
